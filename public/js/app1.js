2ba7d41aea5ce6c223c9119dd9128b2dff8cc81c
ruchikagohil22 <ruchikagohil22@gmail.com>
2020-08-22 10:57:47 +0530

    second commit

--- public/js/app1.js
+++ public/js/app1.js
@@ -1,7 +1,7 @@
 
 
 
-console.log('client side js file is loaded')
+
 // fetch('http://puzzle.mead.io/puzzle').then((response) =>{
 //     response.json().then((data)=>{
 //         console.log(data)
